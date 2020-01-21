a9ad36f7afbdcff9b251e6b24c054f304d482df6
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2020-01-21 22:54:40 +0100

    main updated as per refactored code

--- main.ps1
+++ main.ps1
@@ -1,9 +1,11 @@
 # This is the entry point to run the Circle project. Run this to run the project. 
-$currentDir = Get-Location  # Should give you your current directory where you downloaded this project
-$modules = Get-ChildItem -Path "$currentDir\*.psm1" -Recurse -Force
+
+# Put this script in the root directory of the project
+$rootDir = $PSScriptRoot  # This will always give you the directory of this script (root in this case)
+$modules = Get-ChildItem -Path "$rootDir\*.psm1" -Recurse -Force
 foreach($module in $modules){
     Import-Module $module -Force
 }
 
-# You can change the file path below to either run with your real data (say PersonalCallLogs.csv) or with dummy data (OriginalPDF2CSVConvertedFile.csv)
-Get-Circle -PathOfInputCSVFile "$currentDir\TestData\OriginalPDF2CSVConvertedFile.csv" -SaveAsJSON -SaveAsCSV -SaveAsTable -Verbose
+# You only need to pass the root directory information. Rest all paths are used relative to root directory.
+Get-Circle -RootDir "$rootDir" -SaveAsJSON -SaveAsCSV -SaveAsTable -Verbose
