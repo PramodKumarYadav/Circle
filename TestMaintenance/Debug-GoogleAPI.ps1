5d12100d855592f1e74dac064fe9af5bb4ac2dd0
Pramod Kumar Yadav <p.yadav@payvision.com>
2020-01-24 15:08:47 +0100

    From experience I see we need multiple scripts for debugging. These are to call scripts with frequently used data.

---
+++ TestMaintenance/Debug-GoogleAPI.ps1
@@ -0,0 +1,12 @@
+# Test the working of individual functions and overall scripts here
+
+# modules are scoped to the script, so we have to do this here. Cannot do it in say a function. 
+$thisDir = $PSScriptRoot  # This will always give you the directory of this script (root in this case)
+$rootDir = Split-Path -Path $thisDir
+$modules = Get-ChildItem -Path "$rootDir\*.psm1" -Recurse -Force
+foreach($module in $modules){
+    Import-Module $module -Force
+}
+
+# Give name of functions that you want to call here. example is given below.
+Select-DataRecords
